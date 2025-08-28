e24d47995b2512c2b5a87f89046c77a1d61c6a6e
razerrr15 <raihanreza564@gmail.com>
2025-08-28 13:26:52 +0900

    commit kedua

---
+++ tugas_satu/bin/latihan/latihan2.dart
@@ -0,0 +1,12 @@
+void main() {
+  num panjang = 10;
+  num lebar = 5;
+
+  num luas = panjang * lebar;
+  num keliling = 2 * (panjang + lebar);
+
+  print('Panjang = $panjang');
+  print('Lebar = $lebar');   
+  print('Luas = $luas');
+  print('Keliling = $keliling');
+}
